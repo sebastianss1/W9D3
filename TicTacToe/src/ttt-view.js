a42d472b7534659d56d28d8f2680b439f3ffccfe
juliajykim <jkim24@gwu.edu>
2021-04-21 17:26:24 -0400

    Finish TTT game

--- TicTacToe/src/ttt-view.js
+++ TicTacToe/src/ttt-view.js
@@ -2,14 +2,14 @@ class View {
   constructor(game, $el) {
     this.game = game;
     this.$el = $el;
-    this.setupBoard(); 
+    this.setupBoard();
     this.bindEvents();
   }
 
   bindEvents() {
     this.$el.on('click', 'li', (callback => {
-    const $target = $(callback.currentTarget);
-    this.makeMove($target);
+      const $target = $(callback.currentTarget);
+      this.makeMove($target);
     }));
   }
 
@@ -17,38 +17,39 @@ class View {
     const player = this.game.currentPlayer;
     const position = $square.data('pos');
 
-    try { 
-    // debugger
-    this.game.playMove(position);
+    try {
+      // debugger
+      this.game.playMove(position);
     } catch (err) {
-     alert('Move was invalid');
-    return;
+      alert('Move was invalid');
+      return;
     }
 
-    $square.addClass(player);
+    $square.addClass(player); // x ,o 
 
     if (this.game.isOver()) {
       this.$el.off('click');
-      this.$el.addClass('game-over') 
 
-      let winner = this.game.winner();
+      this.$el.addClass('game-over');
+
+      let winner = this.game.winner(); //winner's mark => x / o
+      const $figcap = $("<figcaption>");
 
       if (winner) {
-        $(`${winner}`).addClass('winner')
-        console.log(`You Win, ${winner}`)
+        this.$el.addClass(`winner-${winner}`);
+        $figcap.html(`You Win, ${winner}`);
       } else {
-        console.log("It's a draw!")
+        $figcap.html("It's a draw!");
       };
-
-    
+      this.$el.append($figcap);
 
 
       // if winner 
-        // add winner class (everything turns green etc )
-        // you win, winner printed 
+      // add winner class (everything turns green etc )
+      // you win, winner printed 
       // else (draw)
-        // add draw class (everything turns red)
-        // prints "it's a draw!"
+      // add draw class (everything turns red)
+      // prints "it's a draw!"
       // end 
     }
 
@@ -60,7 +61,7 @@ class View {
     for (let row = 0; row < 3; row++) {
       for (let col = 0; col < 3; col++) {
         const $li = $('<li>');
-        $li.data("pos",[row, col]);
+        $li.data("pos", [row, col]);
         $ul.append($li);
       }
     }
